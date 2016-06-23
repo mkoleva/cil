967bc98bf980a969b1289a4a553dc4b66b770c63
KarimLabib <karim.rushdi@gmail.com>
2016-06-24 01:47:54 +0200

    added rbm with 100 hidden states to linear regression.
    Also corrected naming convention for data files

--- project1/linear_regression.py
+++ project1/linear_regression.py
@@ -6,23 +6,27 @@ from sklearn import linear_model, cross_validation, ensemble, svm
 def train():
 
     feature_vector_svd = np.load('data/svd/feature_vector_svd.npy')
-    feature_vector_sgd = np.load('data/sgd/feature_vector_sgd_220.npy')
-    feature_vector_sgd_c_80 = np.load('data/sgd/feature_vector_sgd_c_80.npy')
+    feature_vector_sgd_c_40 = np.load('data/sgd/feature_vector_sgd_40.npy')
+    feature_vector_sgd_c_80 = np.load('data/sgd/feature_vector_sgd_80.npy')
+    feature_vector_sgd_c_100 = np.load('data/sgd/feature_vector_sgd_100.npy')
     feature_vector_knn = np.load('data/knn/feature_vector_knn.npy')
     feature_vector_knn_items = np.load('data/knn/feature_vector_knn_items.npy')
-    feature_vector_rbm = np.load('data/rbm/feature_vector_rbm.npy')
+    feature_vector_rbm_80 = np.load('data/rbm/feature_vector_rbm_80.npy')
+    feature_vector_rbm_100 = np.load('data/rbm/feature_vector_rbm_100.npy')
 
     X, y = [], []
 
     for i in range(0, np.shape(feature_vector_knn)[0]):
         X.append([feature_vector_svd[i][0],
-                  feature_vector_sgd[i][0],
+                  feature_vector_sgd_c_40[i][0],
                   feature_vector_sgd_c_80[i][0],
+                  feature_vector_sgd_c_100[i][0],
                   feature_vector_knn[i][0],
                   feature_vector_knn_items[i][0],
-                  feature_vector_rbm[i][0]])
+                  feature_vector_rbm_80[i][0],
+                  feature_vector_rbm_100[i][0]])
 
-        y.append(feature_vector_sgd[i][1])
+        y.append(feature_vector_svd[i][1])
 
     regressor = linear_model.LinearRegression()
     regressor.fit(X, y)
@@ -38,11 +42,13 @@ def train():
 def predict(regressor):
 
     svd = np.genfromtxt('data/svd/my_prediction_svd.csv', delimiter=',', dtype=None)
-    sgd = np.genfromtxt('data/sgd/my_prediction_sgd_220.csv', delimiter=',', dtype=None)
-    sgd_c_80 = np.genfromtxt('data/sgd/my_prediction_sgd_c_80.csv', delimiter=',', dtype=None)
+    sgd_c_40 = np.genfromtxt('data/sgd/my_prediction_sgd_40.csv', delimiter=',', dtype=None)
+    sgd_c_80 = np.genfromtxt('data/sgd/my_prediction_sgd_80.csv', delimiter=',', dtype=None)
+    sgd_c_100 = np.genfromtxt('data/sgd/my_prediction_sgd_80.csv', delimiter=',', dtype=None)
     knn = np.genfromtxt('data/knn/my_prediction_knn.csv', delimiter=',', dtype=None)
     knn_items = np.genfromtxt('data/knn/my_prediction_knn_items.csv', delimiter=',', dtype=None)
-    rbm = np.genfromtxt('data/rbm/my_prediction_rbm.csv', delimiter=',', dtype=None)
+    rbm_80 = np.genfromtxt('data/rbm/my_prediction_rbm_80.csv', delimiter=',', dtype=None)
+    rbm_100 = np.genfromtxt('data/rbm/my_prediction_rbm_100.csv', delimiter=',', dtype=None)
 
     f = open('data/my_prediction_linear_regression.csv', 'w')
 
@@ -50,27 +56,31 @@ def predict(regressor):
 
     print "prediction"
 
-    for i in range(1, np.shape(sgd)[0]):
+    for i in range(1, np.shape(svd)[0]):
         if i % 1000 == 0:
             print i
 
-        entry = sgd[i][0].split("_")
+        entry = svd[i][0].split("_")
         row = int(entry[0][1:])
         column = int(entry[1][1:])
 
         svd_rating = float(svd[i][1])
-        sgd_rating = float(sgd[i][1])
+        sgd_40_rating = float(sgd_c_40[i][1])
         sgd_80_rating = float(sgd_c_80[i][1])
+        sgd_100_rating = float(sgd_c_100[i][1])
         knn_rating = float(knn[i][1])
         knn_items_rating = float(knn_items[i][1])
-        rbm_rating = float(rbm[i][1])
+        rbm_80_rating = float(rbm_80[i][1])
+        rbm_100_rating = float(rbm_100[i][1])
 
         prediction = regressor.predict(np.array([[svd_rating,
-                                                  sgd_rating,
+                                                  sgd_40_rating,
                                                   sgd_80_rating,
+                                                  sgd_100_rating,
                                                   knn_rating,
                                                   knn_items_rating,
-                                                  rbm_rating]]))
+                                                  rbm_80_rating,
+                                                  rbm_100_rating]]))
 
         if prediction > 5:
             prediction = 5
